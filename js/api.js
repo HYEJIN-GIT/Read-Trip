60ddd0df6b6b6d0cd2bd99fba0d7d4de86345e98
HYEJIN KWON <chahaa0189@gmail.com>
2026-03-16 16:20:15 +0900

    feat : pagination 기능 추가 구현

--- js/api.js
+++ js/api.js
@@ -5,9 +5,9 @@ import { API_KEY } from "./config.js";
 
 // 서치 api 넘겨주기
 
-export const searchBooks = async (keyword) => {
+export const searchBooks = async (keyword,page) => {
 
-   let url = `https://dapi.kakao.com/v3/search/book?query=${keyword}`;
+   let url = `https://dapi.kakao.com/v3/search/book?query=${keyword}&page=${page}&size=10`;
   
     const response = await fetch(url, {
       headers: {
--- js/search.js
+++ js/search.js
@@ -2,10 +2,14 @@ import {searchBooks} from "./api.js"
 
 
 const userValue = document.getElementById('user-value')
-const button = document.getElementById('btn')
+// const button = document.getElementById('btn')
 const recommend = ["해리포터","민음사","안전가옥","히가시노 게이고","한강","프로젝트 헤일메리"]
 const text =  document.querySelector('.search-text h4')
 let bookList = []
+let page = 1
+let totalResult = 0
+const size = 10
+const groupSize = 5
 
 
 // 버튼 클릭 이벤트
@@ -26,10 +30,13 @@ userValue.addEventListener("keydown",(event)=>{
 
 const getSearchBooks = async ()=>{
     let keyword = userValue.value.trim()
-    let data = await searchBooks(keyword)
+    let data = await searchBooks(keyword,page)
+    console.log("요청 페이지", page)
     bookList = data.documents
+    totalResult = data.meta.total_count
     console.log(data)
     render()
+    paginationRender()
 }
 
 // 추천 키워드 렌더
@@ -83,4 +90,89 @@ const render = ()=>{
     `
  
     document.getElementById('book-render').innerHTML = bookHTML
+}
+
+
+
+// 페이지네이션 렌더
+
+const paginationRender = ()=>{
+  let totalPage = Math.ceil(totalResult/size)
+  let pageGroup = Math.ceil(page/groupSize)
+  let lastPage = pageGroup*groupSize
+  if(lastPage>totalPage){
+    lastPage=totalPage
+  }
+  let firstPage = lastPage -(groupSize-1) < 1? 1 : lastPage -(groupSize-1)
+  console.log(lastPage,firstPage)
+
+
+
+  let paginationHTML = ``
+if(page>1){
+  paginationHTML=`
+  <li class="page-item"><a class=" page-link prev">&lt;&lt;</a></li>
+  <li class="page-item"><a class=" page-link prev">&lt;</a></li>`
+  
+}
+
+  for(let i =firstPage;i<=lastPage;i++){
+    paginationHTML+= `
+   
+    <li class="page-item ${i===page?"active":""}"><a class="page-link">${i}</a></li>
+  `
+  }
+  if(page<totalPage){
+paginationHTML+=`<li class="page-item"><a class=" page-link prev">&gt;</a></li>
+  <li class="page-item"><a class=" page-link prev">&gt;&gt;</a></li>`
+  }
+  
+
+  document.querySelector('.pagination').innerHTML = paginationHTML
+  
+
+  moveToPage(totalPage)
+
+
+}
+
+const  moveToPage = (totalPage) => {
+
+  const buttons = document.querySelectorAll(".page-link")
+
+  buttons.forEach((btn)=>{
+
+    btn.addEventListener("click",()=>{
+
+      let text = btn.textContent
+
+      // 이전 버튼
+      if(text === "<<"){
+        page =1
+      } else if(text === "<"){
+        if(page > 1){
+          page--
+        }
+
+      // 다음 버튼
+      }
+      else if(text ===">>"){
+        page = totalPage
+      }     
+      else if(text === ">"){
+        if(page < totalPage){
+          page++
+        }
+
+      // 숫자 버튼
+      }else{
+        page = Number(text)
+      }
+
+      getSearchBooks()
+
+    })
+
+  })
+
 }
