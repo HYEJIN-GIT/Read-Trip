10967759987a7268f13cd1cb15ac72bf849c2303
HYEJIN KWON <chahaa0189@gmail.com>
2026-03-20 00:24:18 +0900

    feat : 서치창 기능 구현 복습 및 추천키워드 기능 구현 복습

--- js/home.js
+++ js/home.js
@@ -9,7 +9,6 @@ let start = 0
 const getBestSell = async ()=>{
     let data = await bestSellBooks()
     bestBookList = data.documents
-    console.log("베ㅐ스트북 배열 5개만 보여주기;",bestBookList.slice(0,5))
     console.log(bestBookList)
     render()
 }
@@ -82,31 +81,3 @@ document.getElementById('right').addEventListener('click',()=>{
 
 
 
-
-// getBestSell()
-
-// const render = ()=>
-// {
-//     let bestHTML = 
-    
-//     `<section class ="search-book-area row">
-//     ${
-//         bestBookList.map(best=>
-
-//             `
-            
-//                 <div class = "col-2">
-                  
-//                     <img src ="${best.thumbnail}">
-//                      <div class="book-title">${best.title.length <10 ? best.title: best.title.slice(0,10)+"..."}</div>
-//                      <div class ="book-authors">${best.authors}</div>
-//                 </div>
-//             `
-//         ).join('')
-//     }
-    
-    
-//     </section>`
-  
-//     document.getElementById('best-sell').innerHTML = bestHTML
-// }
